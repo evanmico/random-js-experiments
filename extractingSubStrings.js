3d729429b4388cbdac128db0432de474a2a9e7b8
evanmico <evanmico222@gmail.com>
2025-05-31 22:29:03 -0400

    did some look ahead and look behind practice with regex and more

---
+++ extractingSubStrings.js
@@ -0,0 +1,14 @@
+const startIndexes = [1, 2, 1];
+const endIndexes = [8, 5, 3];
+const s = '*|**|*|*'
+// find how many * are enclosed in | between each of the starting and ending indexes
+const findNumStars = (s, startIndexes, endIndexes) => {
+    return startIndexes.map((start, idx) => {
+        let end = endIndexes[idx];
+        let matchRegex = /(?<=\|)\*+(?=\|)/g;
+        let matches =  s.substring(start-1,end).match(matchRegex);
+        return (matches === null)?(0):matches.reduce((a,b) => (a+b)).length;
+    })
+}
+
+console.log(findNumStars(s, startIndexes, endIndexes));
